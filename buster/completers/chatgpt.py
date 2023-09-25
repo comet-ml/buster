c37536df5913a6f144e15a570be5717e955c7551
Boris Feld <lothiraldan@gmail.com>
2023-09-25 18:36:42 +0200

    Rely on OpenAI integration to log OpenAI calls

--- buster/completers/chatgpt.py
+++ buster/completers/chatgpt.py
@@ -21,24 +21,21 @@ class ChatGPTCompleter(Completer):
         inputs = completion_kwargs.copy()
         inputs["messages"] = messages
 
-        with Span(inputs, "llm-call", metadata={"stream": stream}) as span:
-            response = openai.ChatCompletion.create(
-                messages=messages,
-                **completion_kwargs,
-            )
+        response = openai.ChatCompletion.create(
+            messages=messages,
+            **completion_kwargs,
+        )
 
-            if stream is True:
-                # We are entering streaming mode, so here were just wrapping the streamed
-                # openai response to be easier to handle later
-                def answer_generator():
-                    for chunk in response:
-                        token: str = chunk["choices"][0]["delta"].get("content", "")
-                        yield token
+        if stream is True:
+            # We are entering streaming mode, so here were just wrapping the streamed
+            # openai response to be easier to handle later
+            def answer_generator():
+                for chunk in response:
+                    token: str = chunk["choices"][0]["delta"].get("content", "")
+                    yield token
 
-                span.set_outputs(outputs={"output": "stream"})
-                return answer_generator()
+            return answer_generator()
 
-            else:
-                full_response: str = response["choices"][0]["message"]["content"]
-                span.set_outputs(outputs={"output": full_response})
-                return full_response
+        else:
+            full_response: str = response["choices"][0]["message"]["content"]
+            return full_response
